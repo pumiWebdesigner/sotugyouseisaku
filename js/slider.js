055e83cd63f2a850e0dd7d6bc3874bfd4b1e94e6
pumiWebdesigner <pumi.webdesigner@gmail.com>
2024-02-27 22:30:01 +0900

    mv pc完成

--- js/slider.js
+++ js/slider.js
@@ -1,20 +1,24 @@
 const swiper = new Swiper(".swiper", {
   speed: 500, // 次の画像を読み込む速さ
   loop: true,
-  loopedSlides: 3,
+  loopedSlides: 6,
+  spaceBetween: 0,
+
   autoplay: {
-    delay: 5000, // 次の画像に切替する時間
+    delay: 500000, // 次の画像に切替する時間
     disableOnInteraction: false, // スライドのドラッグ等の操作時でも自動再生を無効にしない：false
   },
   effect: "fade", // フワッと切替
   fadeEffect: {
     crossFade: true, // 画像が重ならないように制御
   },
+  // ボタンタイプのページネーション
   pagination: {
     el: ".swiper-pagination",
     type: "bullets",
     clickable: true,
   },
+  // 左右の矢印アイコン
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
@@ -26,6 +30,6 @@ const swiper = new Swiper(".swiper", {
 jQuery(".js-modal__btn--open").on("click", function () {
   swiper.autoplay.stop(); // モーダル表示時にスライドを止める
 });
-jQuery(".js-modal__btn--close").on("click", function (e) {
+jQuery(".js-modal__btn--close").on("click", function () {
   swiper.autoplay.start(); // モーダルを閉じたらスライドを再開する
 });
