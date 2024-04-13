54f83e5c91b67c4bd3387cfe81e15b1252bbf4e0
pumiWebdesigner <pumi.webdesigner@gmail.com>
2024-04-13 22:57:13 +0900

    link brogまで

--- js/script.js
+++ js/script.js
@@ -1,46 +1,61 @@
 {
   // ページ外からの遷移
   // ページがロードされた後にhash有無をチェックして処理
+  // DOM準備完了時点なら↓でもOK
   $(document).ready(function () {
-    if (window.location.hash) {
-      // URLのhashが遷移する先（例：#access）
-      var hash = window.location.hash; // #～を取得
-      // 遷移する先とheaderの高さからスクロールする距離を計算
-      scrollDistance = calcDistance(hash);
+    ////// 画像含めてリソースがロードされてからなら↓
+    ////// $(window).on('load', function () {
+    // URLのhashが遷移する先（例：#access）
+    var hash = window.location.hash; // #～を取得
+    // URLにhashがあるかチェック
+    console.log(hash);
+    if (hash) {
+      var scrollDistance = 0; // スクロール先までの距離（#は初期値0）
+      // hashが存在し、かつ#でない場合のみスクロールする距離を計算する
+      if ($(hash).length > 0) {
+        // 遷移する先とheaderの高さからスクロールする距離を計算
+        scrollDistance = calcDistance(hash);
+      }
+      console.log(scrollDistance);
       // スムーズスクロール
-      smoothScroll(scrollDistance, 0);
+      smoothScroll(scrollDistance, 300);
     }
   });
 
+  // a[href*="#"]：href属性に#が含まれるもの
+  // ページ内のindex.html#〜も対象にしようとしたが、pathの正規か判定が難しいので断念
+  // var hashIndex = href.indexOf("#"); // # の位置を取得 (# が存在しない場合は -1 を返す)
+  // hrefのpath（＃前の文字列）を取得する
+  // var thisPath = href.substring(0, hashIndex);
+  // 現在のページのpathを取得する
+  // var locationPath = window.location.pathname;
+  // var locationPath = window.location.origin + window.location.pathname;
+  // ページ内リンクの場合のみを対象とする
+  // if (thisPath === locationPath || thisPath === "") {
+  // }
+  // ページ外への遷移は別途記載
+
   // ページ内の遷移
-  // aタグのクリック（ドロワー関連は専用処理あり）
+  // a[href^="#"]：href属性が#から始まるもの
   jQuery('a[href^="#"]').on("click", function (e) {
-    e.preventDefault(); // aタグの通常の処理を止める
-    // aタグのhrefが遷移する先
-    var id = jQuery(this).attr("href"); // スクロール先のhrefを取得
-    // 遷移する先とheaderの高さからスクロールする距離を計算
-    scrollDistance = calcDistance(id);
+    // #を含む場合はスムーズスクロールしたいので通常の処理を止める⇔含まないなら通常の処理を行う
+    e.preventDefault();
+    var hash = jQuery(this).attr("href"); // hrefから #～を取得
+    var scrollDistance = 0; // スクロール先までの距離（#は初期値0）
+    // hashが存在し、かつ#でない場合のみスクロールする距離を計算する
+    if (hash != "#") {
+      // 遷移する先とheaderの高さからスクロールする距離を計算
+      scrollDistance = calcDistance(hash);
+      console.log(scrollDistance);
+    }
     // スムーズスクロール
     smoothScroll(scrollDistance, 300);
-    // ドロワーを閉じる
-    if (jQuery(this).hasClass("js-drawer__nav--link")) {
-      jQuery("body").removeClass("drawer-open");
-    }
   });
   // 遷移する先とheaderの高さからスクロールする距離を計算
-  function calcDistance(id) {
-    var scrollDistance = 0; // #は初期値0
-    console.log(id);
-    if (id && id !== "#" && $(id).length) {
-      // idが存在しない場合は$(id)がエラーになるので、idが存在する場合のみ処理を行う
-      // id == "#"の場合、elementDistanceの取得でエラーになるので、#の場合はスクロールしない
-      // ページ内にidと同名のDOM要素の数が1つ以上ある場合のみ処理を行う
-      var elementDistance = $(id).offset().top; //画面最上部から要素の上端の距離
-      var headerHeight = $(".header").outerHeight(); // ヘッダーの高さ（マージン含む）
-      scrollDistance = elementDistance - headerHeight; // ヘッダーの高さを考慮した位置にスクロール
-      scrollDistance -= 20; // 20px余裕を持たせる
-    }
-    return scrollDistance;
+  function calcDistance(hash) {
+    var elementDistance = $(hash).offset().top; //要素の位置（画面最上部から要素上端の距離）
+    var headerHeight = $(".header").outerHeight(true); // ヘッダーの高さ（マージン含む）
+    return elementDistance - headerHeight - 20; // 20px余裕を持たせる
   }
   // 距離とスピードを渡してスムーズスクロール
   function smoothScroll(scrollDistance, speed) {
@@ -57,6 +72,10 @@
     e.preventDefault();
     jQuery("body").toggleClass("drawer-open");
   });
+  // ドロワーを閉じる
+  if (jQuery(this).hasClass("js-drawer__nav--link")) {
+    jQuery("body").removeClass("drawer-open");
+  }
 
   //スクロール後処理__画面最上部からtargetの場所を超えたら起動
   jQuery(window).on("scroll", function () {
